2be43dd33ce6a5092e17e4e7f62c0baaf12f4919
SomeProgrammer <92398408+SomePogProgrammer@users.noreply.github.com>
2023-08-30 16:14:11 +0000

    header,js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 /* © | Copyright 2023, SomeProgrammer's ReactJS Portfolio | SomePogProgrammer | ALL RIGHTS RESERVED © */
-import React from "react";
+
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./index.css";
-import Login from "./views/login";
-import Home from "./views/home";
-import ContactMe from "./views/contact-me";
-import PastWorkHTML from "./views/past-work";
-import BehindTheScenes from "./views/behind-the-scenes";
-import Page from "./views/page";
-import AboutMe from "./views/about-me";
-import WebDevelopment from "./views/web-development";
+import Login from "./pages/login";
+import Home from "./pages/home";
+import ContactMe from "./pages/contact-me";
+import PastWorkHTML from "./pages/past-work";
+import BehindTheScenes from "./pages/behind-the-scenes";
+import Page from "./pages/page";
+import AboutMe from "./pages/about-me";
+import WebDevelopment from "./pages/web-development";
 import { fixComponentProps } from "./componentFunctions.js/compFuncs";
 window.fixComponentProps = fixComponentProps;
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Login />} path="https://qhkvxj.csb.app/login" />
+        <Route element={<Login />} path="login" />
         <Route element={<Home />} path="/home" />
         <Route element={<ContactMe />} path="/contact-me" />
         <Route element={<PastWorkHTML />} path="/past-work" />
