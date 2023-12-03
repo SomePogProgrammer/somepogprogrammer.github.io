920f747c26bce0f9d1ae7e1a4f5daf64f4ac5f2c
SomeProgrammer <92398408+SomePogProgrammer@users.noreply.github.com>
2023-12-03 12:56:13 -0500

    _

--- app/utils/getDBref.ts
+++ app/utils/getDBref.ts
@@ -5,11 +5,12 @@ import admin from "firebase-admin"
 export async function getRef() {
     var fet = await fetch("https://portfolio-api-backend.vercel.app/server/api/getDB",{headers: {auth : "RnJvbSBzb21lcG9ncHJvZ3JhbW1lci5naXRodWIuaW8="}}).then(async (res) => { return await res.json() })
     if (!admin.apps.length) {
+        console.log(fet)
         admin.initializeApp({
             
             credential: admin.credential.cert(fet), databaseURL: "https://portfolio-api-and-backend-default-rtdb.firebaseio.com/",
         })
-        console.log(fet)
+
     }
    
     var database = getDatabase()
