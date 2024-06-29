e1ddf968c646cce311e0689024fd94767bc16d41
SomeProgrammer <92398408+SomePogProgrammer@users.noreply.github.com>
2024-06-29 11:57:59 -0400

    Update getDBref.ts
    Signed-off-by: SomeProgrammer <92398408+SomePogProgrammer@users.noreply.github.com>

--- app/utils/getDBref.ts
+++ app/utils/getDBref.ts
@@ -5,7 +5,6 @@ import admin from "firebase-admin"
 export async function getRef() {
     var fet = await fetch("https://portfolio-api-backend.vercel.app/server/api/getDB",{headers: {auth : "RnJvbSBzb21lcG9ncHJvZ3JhbW1lci5naXRodWIuaW8="}}).then(async (res) => { return await res.json() })
     if (!admin.apps.length) {
-        console.log(fet)
         admin.initializeApp({
             
             credential: admin.credential.cert(fet), databaseURL: "https://portfolio-api-and-backend-default-rtdb.firebaseio.com/",
