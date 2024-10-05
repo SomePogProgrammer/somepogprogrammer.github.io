e2fe56dd87aae22ce610fcde4411dda896f0993b
SomeProgrammer <92398408+SomePogProgrammer@users.noreply.github.com>
2024-10-05 16:45:50 -0400

    readded debug

--- app/utils/getDBref.ts
+++ app/utils/getDBref.ts
@@ -3,6 +3,7 @@ import { getDatabase } from "firebase-admin/database"
 import admin from "firebase-admin"
 
 export async function getRef() {
+    console.log("process",process.env.API_AUTH_KEY)
     var fet = await fetch("https://portfolio-api-backend.vercel.app/server/api/getDB",{headers: {auth : process.env.API_AUTH_KEY as string}}).then(async (res) => { return await res.json() })
     if (!admin.apps.length) {
         admin.initializeApp({
